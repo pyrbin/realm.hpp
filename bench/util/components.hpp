454c03eb3b0260452f7268866bf37cf14787907e
pyrbin <31902695+pyrbin@users.noreply.github.com>
2020-02-11 05:24:56 +0100

    added simple benchmarks

---
+++ bench/util/components.hpp
@@ -0,0 +1,19 @@
+struct pos
+{
+    float x = 0.0f;
+    float y = 0.0f;
+};
+
+struct dir
+{
+    float x = 4.0f;
+    float y = 0.0f;
+};
+
+struct wierd
+{
+    float thingy = 0.0;
+    int dingy = 0;
+    bool mingy = false;
+    std::string stringy;
+};
---
+++ bench/util/timer.hpp
@@ -0,0 +1,13 @@
+struct timer final
+{
+    timer() : start{ std::chrono::system_clock::now() } {}
+
+    double elapsed()
+    {
+        auto now = std::chrono::system_clock::now();
+        return std::chrono::duration<double>(now - start).count();
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> start;
+};
