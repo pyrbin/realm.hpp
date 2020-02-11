015bd99989192ac6f24b03c78867aacf7c9dceae
pyrbin <31902695+pyrbin@users.noreply.github.com>
2020-02-11 03:44:09 +0100

    updated test cmake & added catch2

---
+++ test/catch.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// TODO: is this a good way to configure my tests?
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "../include/realm.hpp"
+#include "utils/components.hpp"
---
+++ test/utils/components.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+struct pos
+{
+    float x, y, z = 0;
+};
+
+struct vel
+{
+    float x, y, z = 0;
+};
+
+struct name
+{
+    std::string value = "";
+};
