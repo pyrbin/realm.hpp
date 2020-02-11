0d085f799f14d8c21068ccba07c1332729264ea5
pyrbin <31902695+pyrbin@users.noreply.github.com>
2020-02-11 04:06:53 +0100

    removed clutter

--- include/internal/type_traits.hpp
+++ include/internal/type_traits.hpp
@@ -14,28 +14,29 @@ template<typename T, typename... Rest>
 constexpr bool is_unique<T, Rest...> =
   std::bool_constant<(!std::is_same_v<T, Rest> && ...) && is_unique<Rest...>>{};
 
+
 template<typename T>
 inline constexpr bool is_component = (std::is_class_v<T> &&
                                       std::is_copy_constructible_v<T> &&
                                       std::is_move_constructible_v<T> &&
                                       !std::is_const_v<T>);
-
 template<typename... T>
 inline constexpr bool is_component_pack =
-  internal::is_unique<std::remove_const_t<std::unwrap_ref_decay_t<T>>...> &&
-  (internal::is_component<T>, ...);
+  is_unique<std::remove_const_t<std::unwrap_ref_decay_t<T>>...> &&
+  (... && is_component<T>);
 
 template<typename T>
 inline constexpr bool is_entity = std::is_integral_v<T>;
 
 template<typename T, typename R = T>
-using enable_if_component = std::enable_if_t<internal::is_component<T>, R>;
+using enable_if_component = std::enable_if_t<is_component<T>, R>;
 
 template<typename T, typename R = T>
-using enable_if_entity = std::enable_if_t<internal::is_entity<T>, R>;
+using enable_if_entity = std::enable_if_t<is_entity<T>, R>;
 
 template<typename R, typename... T>
-using enable_if_component_pack = std::enable_if_t<internal::is_component_pack<T...>, R>;
+using enable_if_component_pack = std::enable_if_t<is_component_pack<T...>, R>;
+
 
 } // namespace internal
 
